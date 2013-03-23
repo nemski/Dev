3168d9b285539c32a5c715e6d3682074604e588a
Pat <nemski.rabbit@gmail.com>
2013-03-24 02:24:19 +1100

    calculate authKey
    We can now calculate authKey

--- snmpv3-scanner.rb
+++ snmpv3-scanner.rb
@@ -2,6 +2,55 @@
 require 'openssl'
 require 'socket'
 
+class OpenSSL::Digest::MD5
+	# A Ruby implementation of http://tools.ietf.org/html/rfc3414#appendix-A.2.1
+	def usmHMACMD5AuthKey(authPass, msgAuthEngineID)
+		@count = 0
+		@password_buf = Array.new(64)
+		@password_index = 0
+
+			begin
+				@password_index += 1
+				@password_buf.each do |i|
+					i = authPass[@password_index % authPass.length]
+				end
+
+				self << @password_buf.to_s
+				@count += 64
+			end until @count >= 1048576
+
+			@password_buf = "self.digest + msgAuthEngineID + self.digest"
+
+			self << @password_buf.to_s
+			self.digest
+	end
+
+	def usmHMACMD5AuthProtocol(authKey, wholeMsg)
+=begin
+   2) From the secret authKey, two keys K1 and K2 are derived:
+
+      a) extend the authKey to 64 octets by appending 48 zero octets;
+         save it as extendedAuthKey
+
+      b) obtain IPAD by replicating the octet 0x36 64 times;
+
+      c) obtain K1 by XORing extendedAuthKey with IPAD;
+
+      d) obtain OPAD by replicating the octet 0x5C 64 times;
+
+      e) obtain K2 by XORing extendedAuthKey with OPAD.
+
+   3) Prepend K1 to the wholeMsg and calculate MD5 digest over it
+      according to [RFC1321].
+
+   4) Prepend K2 to the result of the step 4 and calculate MD5 digest
+      over it according to [RFC1321].  Take the first 12 octets of the
+      final digest - this is Message Authentication Code (MAC).
+=end
+
+	end
+end
+
 # SNMP datatypes
 
 INTEGER                 = "\x02"
@@ -22,148 +71,94 @@ MSG_ID                  = "\x01"
 MSG_MAX_SIZE            = "\x20\x00"    # 8192
 MSG_SECURITY_MODEL      = "\x03"        # usmSecurityModel
 
-class Password_to_key_md5
-        require 'inline'
-
-        inline do |builder|
-                # Generate an MD5 Sum as defined by RFC3414
-                builder.c <<-EOC
-                #include <sys/types.h>
-                #include <md5.h>
-
-                void to_md5(
-                        u_char *password,    /* IN */
-                        u_int   passwordlen, /* IN */
-                        u_char *engineID,    /* IN  - pointer to snmpEngineID  */
-                        u_int   engineLength,/* IN  - length of snmpEngineID */
-                        u_char *key)         /* OUT - pointer to caller 16-octet buffer */
-                {
-                        MD5_CTX     MD;
-                        u_char     *cp, password_buf[64];
-                        u_long      password_index = 0;
-                        u_long      count = 0, i;
-
-                        MD5Init (&MD);   /* initialize MD5 */
-
-                        /**********************************************/
-                        /* Use while loop until we've done 1 Megabyte */
-                        /**********************************************/
-                        while (count < 1048576) {
-                                cp = password_buf;
-                                for (i = 0; i < 64; i++) {
-                                        /*************************************************/
-                                        /* Take the next octet of the password, wrapping */
-                                        /* to the beginning of the password as necessary.*/
-                                        /*************************************************/
-                                        *cp++ = password[password_index++ % passwordlen];
-                                }
-                                MD5Update (&MD, password_buf, 64);
-                                count += 64;
-                        }
-                        MD5Final (key, &MD);          /* tell MD5 we're done */
-
-                        /*****************************************************/
-                        /* Now localize the key with the engineID and pass   */
-                        /* through MD5 to produce final key                  */
-                        /* May want to ensure that engineLength <= 32,       */
-                        /* otherwise need to use a buffer larger than 64     */
-                        /*****************************************************/
-                        memcpy(password_buf, key, 16);
-                        memcpy(password_buf+16, engineID, engineLength);
-                        memcpy(password_buf+16+engineLength, key, 16);
-
-                        MD5Init(&MD);
-                        MD5Update(&MD, password_buf, 32+engineLength);
-                        MD5Final(key, &MD);
-                        return;
-                }
-                EOC
-        end
-end
-
 def create_probe_snmp3(msgFlags, userName, authPass, privPass, msgAuthEngineID)
-        msgAuthEngineBoots = "0"
-        msgAuthEngineTime = "0"
-        msgAuthParam = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
-        msgPrivParam = ""
+		msgAuthEngineBoots = "0"
+		msgAuthEngineTime = "0"
+		msgPrivParam = ""
+		msgAuthParam = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
 
-        digest = Password_to_key_md5.new
+		if authPass.length > 0
+			authKey = OpenSSL::Digest::MD5.new
+			authKey.usmHMACMD5AuthKey(authPass, msgAuthEngineID)
+		end
 
-        digest.to_md5(authPass, authPass.length, msgAuthEngineID, msgAuthEngineID.length, msgAuthParam)
+		msgGlobalData =
+				INTEGER + [MSG_ID.length].pack('C') + MSG_ID +
+				INTEGER + [MSG_MAX_SIZE.length].pack('C') + MSG_MAX_SIZE +
+				OCTET_STRING + [msgFlags.length].pack('C') + msgFlags +
+				INTEGER + [MSG_SECURITY_MODEL.length].pack('C') + MSG_SECURITY_MODEL
+		msgGlobalHead = SEQUENCE + [msgGlobalData.length].pack('C')
 
-        msgGlobalData =
-                INTEGER + [MSG_ID.length].pack('C') + MSG_ID +
-                INTEGER + [MSG_MAX_SIZE.length].pack('C') + MSG_MAX_SIZE +
-                OCTET_STRING + [msgFlags.length].pack('C') + msgFlags +
-                INTEGER + [MSG_SECURITY_MODEL.length].pack('C') + MSG_SECURITY_MODEL
-        msgGlobalHead = SEQUENCE + [msgGlobalData.length].pack('C')
+		msgSecurityParameters   =
+				OCTET_STRING + [msgAuthEngineID.length].pack('C') + msgAuthEngineID +
+				INTEGER + [msgAuthEngineBoots.length].pack('C') + msgAuthEngineBoots +
+				INTEGER + [msgAuthEngineTime.length].pack('C') + msgAuthEngineTime +
+				OCTET_STRING + [userName.length].pack('C') + userName +
+				OCTET_STRING + [msgAuthParam.length].pack('C') + msgAuthParam +
+				OCTET_STRING + [msgPrivParam.length].pack('C') + msgPrivParam
+		msgSecurityHead         =
+				OCTET_STRING + [msgSecurityParameters.length + 2].pack('C') +
+				SEQUENCE + [msgSecurityParameters.length].pack('C')
 
-        msgSecurityParameters   =
-                OCTET_STRING + [msgAuthEngineID.length].pack('C') + msgAuthEngineID +
-                INTEGER + [msgAuthEngineBoots.length].pack('C') + msgAuthEngineBoots +
-                INTEGER + [msgAuthEngineTime.length].pack('C') + msgAuthEngineTime +
-                OCTET_STRING + [userName.length].pack('C') + userName +
-                OCTET_STRING + [msgAuthParam.length].pack('C') + msgAuthParam +
-                OCTET_STRING + [msgPrivParam.length].pack('C') + msgPrivParam
-        msgSecurityHead         =
-                OCTET_STRING + [msgSecurityParameters.length + 2].pack('C') +
-                SEQUENCE + [msgSecurityParameters.length].pack('C')
+#		pdu =
+#				"\x30\x12\x04\x00\x04\x00\xa0\x0c\x02\x02\x13\x89\x02\x01" +
+#				"\x00\x02\x01\x00\x30\x00"
+		pdu =
+				"\x30\x14\x04\x00\x04\x00\xa0\x0e" + "\x02\x04\x0e\xf0\xee\x8f\x02\x01" +
+				"\x00\x02\x01\x00\x30\x00"
 
-        pdu =
-                "\x30\x12\x04\x00\x04\x00\xa0\x0c\x02\x02\x13\x89\x02\x01" +
-                "\x00\x02\x01\x00\x30\x00"
+		msgVersion = "\x02\x01\x03"
 
-        msg = msgGlobalHead + msgGlobalData + msgSecurityHead + msgSecurityParameters + pdu
+		msg = msgVersion + msgGlobalHead + msgGlobalData + msgSecurityHead + msgSecurityParameters + pdu
 
-        snmpHead = "\x30" + [msg.length].pack('C')
-        msgVersion = "\x02\x01\x03"
-        puts "Msg length " + msg.length.to_s
-        snmp = snmpHead + msgVersion + msg
-        snmp
+		snmpHead = SEQUENCE + [msg.length].pack('C')
+		puts "Msg length " + msg.length.to_s
+		snmp = snmpHead + msg
+		snmp
 end
 
 def parse_reply(pkt)
-        return if not pkt[1]
+		return if not pkt[1]
 
 #       if(pkt[1] =~ /^::ffff:/)
 #               pkt[1] = pkt[1].sub(/^::ffff:/, '')
 #       end
 
-        asn1 = OpenSSL::ASN1.decode(pkt) rescue nil
-        if(! asn1)
-                puts "Not ASN encoded data"
-                return
-        end
+		asn1 = OpenSSL::ASN1.decode(pkt) rescue nil
+		if(! asn1)
+				puts "Not ASN encoded data"
+				return
+		end
 
-        msgVersion = asn1.value[0]
-        msgGlobalData = asn1.value[1]
+		msgVersion = asn1.value[0]
+		msgGlobalData = asn1.value[1]
 
-        # The usmSecurityParameter is an Octet String whose value is a BER encoded ASN1data class, so it must be unpacked
-        msgSecurityParameter = OpenSSL::ASN1.decode(asn1.value[2].value)
-        msgAuthoritativeEngineID = msgSecurityParameter.value[0].value
+		# The usmSecurityParameter is an Octet String whose value is a BER encoded ASN1data class, so it must be unpacked
+		msgSecurityParameter = OpenSSL::ASN1.decode(asn1.value[2].value)
+		msgAuthoritativeEngineID = msgSecurityParameter.value[0].value
 
-        msgData = asn1.value[3]
-        contextEngineID = msgData.value[0]
-        contextName = msgData.value[1]
+		msgData = asn1.value[3]
+		contextEngineID = msgData.value[0]
+		contextName = msgData.value[1]
 
-        pdu = msgData.value[2]
-        requestId = pdu.value[0]
-        errorStatus = pdu.value[1]
-        errorIndex = pdu.value[2]
-        varBind = pdu.value[3]
+		pdu = msgData.value[2]
+		requestId = pdu.value[0]
+		errorStatus = pdu.value[1]
+		errorIndex = pdu.value[2]
+		varBind = pdu.value[3]
 
-        var = varBind.value[0]
-        oid = var.value[0]
-        val = var.value[1]
+		var = varBind.value[0]
+		oid = var.value[0]
+		val = var.value[1]
 
-        snmpResult = {  "msgAuthoritativeEngineID"      => msgAuthoritativeEngineID,
-                        "contextEngineID"       => contextEngineID,
-                        "errorStatus"           => errorStatus,
-                        "oid"                   => oid,
-                        "val"                   => val,
-                }
+		snmpResult = {  "msgAuthoritativeEngineID"      => msgAuthoritativeEngineID,
+						"contextEngineID"       => contextEngineID,
+						"errorStatus"           => errorStatus,
+						"oid"                   => oid,
+						"val"                   => val,
+				}
 
-        snmpResult
+		snmpResult
 end
 
 rhost = ARGV[1]
@@ -180,7 +175,7 @@ ret, sender = udp_socket.recvfrom(65535)
 
 snmpReturn = parse_reply(ret)
 
-data = create_probe_snmp3((MSG_FLAGS_REPORTABLE + MSG_FLAGS_AUTH), "authOnlyUser", "password", "", snmpReturn["msgAuthoritativeEngineID"])
+data = create_probe_snmp3([MSG_FLAGS_REPORTABLE.unpack('H*').join.to_i + MSG_FLAGS_AUTH.unpack('H*').join.to_i].pack('C'), "authOnlyUser", "password", "", snmpReturn["msgAuthoritativeEngineID"])
 
 udp_socket.send(data, 0, rhost, 161)
 
